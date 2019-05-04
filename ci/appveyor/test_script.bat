157a0bd64d55ea137de9f49eb9aa697e4e89262c
Antoine <end2endzone@gmail.com>
2019-05-04 11:18:07 -0400

    Modified test_script.bat to support both Debug and Release configuration.

--- ci/appveyor/test_script.bat
+++ ci/appveyor/test_script.bat
@@ -10,7 +10,11 @@ echo =======================================================================
 echo Testing project
 echo =======================================================================
 cd /d %APPVEYOR_BUILD_FOLDER%\build\bin\%Configuration%
-shellanything_unittest.exe
+if "%Configuration%" == "Debug" (
+  shellanything_unittest-d.exe
+) else (
+  shellanything_unittest.exe
+)
 
 ::reset error in case of test case fail
 exit /b 0
