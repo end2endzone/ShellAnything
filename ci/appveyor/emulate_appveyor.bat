590a83cabb78729f313d36900b15ad2e3f7bc24c
Antoine <end2endzone@gmail.com>
2019-05-04 10:33:39 -0400

    Fixed compilation on AppVeyor's.

--- ci/appveyor/emulate_appveyor.bat
+++ ci/appveyor/emulate_appveyor.bat
@@ -6,9 +6,9 @@ set APPVEYOR=True
 set APPVEYOR_BUILD_FOLDER=%cd%
 set Configuration=Debug
 set Platform=x64
+set PlatformToolset=v100
 REM set Platform=Win32
 REM set PlatformToolset=Windows7.1SDK
-set PlatformToolset=v100
 
 echo Emulating appveyor's environment.
 echo APPVEYOR_BUILD_FOLDER set to '%APPVEYOR_BUILD_FOLDER%'
