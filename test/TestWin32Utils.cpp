4f63af89c30d674085b789957c05f1e527d208c5
Antoine <end2endzone@gmail.com>
2019-05-18 13:37:24 -0400

    Created unit tests for investigating issue #12

--- test/TestWin32Utils.cpp
+++ test/TestWin32Utils.cpp
@@ -24,6 +24,7 @@
 
 #include "TestWin32Utils.h"
 #include "win32_utils.h"
+#include "rapidassist/filesystem.h"
 #include "rapidassist/gtesthelp.h"
 
 namespace shellanything { namespace test
@@ -65,6 +66,59 @@ namespace shellanything { namespace test
       static const size_t BUFFER_SIZE = 1024;
       char post_filename[BUFFER_SIZE];
       sprintf(post_filename, ".shell32.dll.index%03d.bmp", i);
+      file_path.append(post_filename);
+      win32_utils::CreateBMPFile(file_path.c_str(), hBitmap);
+
+      //delete the bitmap
+      DeleteObject(hBitmap);
+    }
+  }
+  //--------------------------------------------------------------------------------------------------
+  TEST_F(TestWin32Utils, testSymbolicLinksIconTransparencyIssue12)
+  {
+    //https://github.com/end2endzone/ShellAnything/issues/12
+
+    struct QUICK_ICON
+    {
+      const char * path;
+      int index;
+    };
+    static const QUICK_ICON icons[] = {
+      {"C:\\Windows\\System32\\imageres.dll", 154},
+      {"C:\\Windows\\System32\\shell32.dll",   29},
+    };
+    static const size_t num_icons = sizeof(icons)/sizeof(icons[0]);
+
+    for(int i=0; i<num_icons; i++)
+    {
+      const char * icon_path = icons[i].path;
+      const int & index = icons[i].index;
+
+      HICON hIconLarge = NULL;
+      HICON hIconSmall = NULL;
+
+      UINT numIconLoaded = ExtractIconEx( icon_path, index, &hIconLarge, &hIconSmall, 1 );
+      ASSERT_GE( numIconLoaded, 1); //at least 1 icon loaded
+      ASSERT_TRUE( hIconLarge != NULL );
+
+      //Convert the icon to a bitmap (with invisible background)
+      SIZE icon_size = win32_utils::GetIconSize(hIconLarge);
+      HBITMAP hBitmap = win32_utils::CopyAsBitmap(hIconLarge, icon_size.cx, icon_size.cy);
+
+      //Remove the invisible background and replace by red color
+      static const COLORREF color_pink  = RGB(255,  0,255);
+      static const COLORREF color_white = RGB(255,255,255);
+      win32_utils::FillTransparentPixels(hBitmap, color_pink);
+
+      DestroyIcon(hIconLarge);
+      DestroyIcon(hIconSmall);
+
+      //save to a file
+      std::string file_path = ra::gtesthelp::getTestQualifiedName();
+      std::string dll_filename = ra::filesystem::getFilename(icon_path);
+      static const size_t BUFFER_SIZE = 1024;
+      char post_filename[BUFFER_SIZE];
+      sprintf(post_filename, ".%s.index%03d.bmp", dll_filename.c_str(), index);
       file_path.append(post_filename);
       win32_utils::CreateBMPFile(file_path.c_str(), hBitmap);
 
