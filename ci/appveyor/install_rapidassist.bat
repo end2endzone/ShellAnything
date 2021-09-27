547106727e5a62138fc52ea108167e9ce53c47fe
Antoine <end2endzone@gmail.com>
2021-09-27 17:41:51 -0400

    * Using RapidAssist version 0.10.1. * Fixed issue #96: Problems with Files > 2Gb

--- ci/appveyor/install_rapidassist.bat
+++ ci/appveyor/install_rapidassist.bat
@@ -19,8 +19,8 @@ git clone "https://github.com/end2endzone/RapidAssist.git"
 cd RapidAssist
 echo.
 
-echo Checking out version v0.8.1...
-git -c advice.detachedHead=false checkout 0.8.1
+echo Checking out version v0.10.1...
+git -c advice.detachedHead=false checkout 0.10.1
 echo.
 
 echo ============================================================================
