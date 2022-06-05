d27b6b44fa6747ad358273d3f73504a6edca6bf7
Antoine Beauchamp <end2endzone@gmail.com>
2022-06-04 20:37:23 -0400

    Added scripts to install doxygen on system before building.

---
+++ ci/windows/install_doxygen.bat
@@ -0,0 +1,61 @@
+@echo off
+
+:: Install docygen.
+:: https://github.com/doxygen/doxygen
+:: https://www.doxygen.nl/download.html
+
+echo Installing doxygen...
+set DOXYGEN_URL=https://www.doxygen.nl/files/doxygen-1.9.4.windows.x64.bin.zip
+set DOXYGEN_FILE=%TEMP%\doxygen.zip
+set DOXYGEN_INSTALL_DIR=%TEMP%\doxygen
+
+:: Check that 7zip is installed
+echo Searching for 7zip executable...
+set PATH=C:\Program Files\7-Zip;%PATH%
+where 7z.exe 1>NUL 2>NUL
+if errorlevel 1 (
+  echo Error: 7zip not installed on system.
+  exit /B %errorlevel%
+)
+echo 7zip found.
+echo.
+
+:: Download installer
+echo Downloading...
+REM REM powershell -nologo -executionpolicy bypass -command "wget '%DOXYGEN_URL%' -outfile '%DOXYGEN_FILE%'
+REM powershell -nologo -executionpolicy bypass -command "(New-Object System.Net.WebClient).DownloadFile('%DOXYGEN_URL%', '%DOXYGEN_FILE%')"
+REM if errorlevel 1 (
+REM   echo Command failed. Unable to download doxygen binaries.
+REM   exit /B %errorlevel%
+REM )
+REM if NOT EXIST "%DOXYGEN_FILE%" (
+REM   echo Download failed. The file '%DOXYGEN_FILE%' is not found.
+REM   exit /B %errorlevel%
+REM )
+echo done.
+echo.
+
+:: Extracting
+mkdir %TEMP%\doxygen 1>NUL 2>NUL
+echo Extracting content to installation directory...
+7z x %DOXYGEN_FILE% -o%DOXYGEN_INSTALL_DIR% -aoa
+if errorlevel 1 (
+  echo Failed to decompress doxygen binary installer.
+  exit /B %errorlevel%
+)
+echo done.
+echo.
+
+:: Searching for doxygen executable
+echo Searching for doxygen executable...
+set PATH=%DOXYGEN_INSTALL_DIR%;%PATH%
+where doxygen.exe 1>NUL 2>NUL
+if errorlevel 1 (
+  echo Error: Doxygen not installed on system.
+  exit /B %errorlevel%
+)
+echo Doxygen found.
+echo.
+
+echo doxygen was installed on the system without error.
+echo.
