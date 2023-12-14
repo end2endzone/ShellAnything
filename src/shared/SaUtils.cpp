db1a9a38d5ef2436320ac57a3656b48cf3aa6c28
Antoine Beauchamp <end2endzone@gmail.com>
2023-12-14 14:24:34 -0500

    Created new utility functions in SaUtils.h & matching unit tests. #130

--- src/shared/SaUtils.cpp
+++ src/shared/SaUtils.cpp
@@ -176,3 +176,125 @@ bool IsFirstApplicationRun(const std::string& name, const std::string& version)
 
   return first_run;
 }
+
+bool IsPrintable(const char c)
+{
+  if (c >= 32 && c <= 126)
+    return true;
+  if (c == '\r' ||
+      c == '\n' ||
+      c == '\t')
+    return true;
+  return false;
+}
+
+bool IsPrintable(const std::string& value)
+{
+  for (size_t i = 0; i < value.size(); i++)
+  {
+    if (!IsPrintable(value[i]))
+      return false;
+  }
+  return true;
+}
+
+bool IsPrintableUtf8(const std::string& value)
+{
+  const char* str = value.c_str();
+
+  for (size_t i = 0; i < value.size(); i++)
+  {
+    const char* substr = &str[i];
+    size_t code_point_len = GetFirstCodePointLengthUtf8(substr);
+    if (code_point_len == std::string::npos)
+      return false;
+    if (code_point_len == 1)
+    {
+      if (!IsPrintable(substr[0]))
+        return false;
+    }
+    else
+    {
+      // that is a code point
+      i += (code_point_len - 1); // -1 because of the i++
+    }
+  }
+  return true;
+}
+
+size_t GetFirstCodePointLengthUtf8(const char* value)
+{
+  const unsigned char* unsigned_str = (const unsigned char*)value;
+
+  const unsigned char& c1 = unsigned_str[0];
+  unsigned char c2 = unsigned_str[1];
+  unsigned char c3 = unsigned_str[2];
+  unsigned char c4 = unsigned_str[3];
+
+  if (c1 == '\0')
+    return 0;
+
+  //prevent going outside of the string
+  if (c1 == '\0')
+    c2 = c3 = c4 = '\0';
+  else if (c2 == '\0')
+    c3 = c4 = '\0';
+  else if (c3 == '\0')
+    c4 = '\0';
+
+  //size in bytes of the code point
+  size_t n = 1;
+
+  //See http://www.unicode.org/versions/Unicode6.0.0/ch03.pdf, Table 3-7. Well-Formed UTF-8 Byte Sequences
+  // ## | Code Points         | First Byte | Second Byte | Third Byte | Fourth Byte
+  // #1 | U+0000   - U+007F   | 00 - 7F    |             |            | 
+  // #2 | U+0080   - U+07FF   | C2 - DF    | 80 - BF     |            | 
+  // #3 | U+0800   - U+0FFF   | E0         | A0 - BF     | 80 - BF    | 
+  // #4 | U+1000   - U+CFFF   | E1 - EC    | 80 - BF     | 80 - BF    | 
+  // #5 | U+D000   - U+D7FF   | ED         | 80 - 9F     | 80 - BF    | 
+  // #6 | U+E000   - U+FFFF   | EE - EF    | 80 - BF     | 80 - BF    | 
+  // #7 | U+10000  - U+3FFFF  | F0         | 90 - BF     | 80 - BF    | 80 - BF
+  // #8 | U+40000  - U+FFFFF  | F1 - F3    | 80 - BF     | 80 - BF    | 80 - BF
+  // #9 | U+100000 - U+10FFFF | F4         | 80 - 8F     | 80 - BF    | 80 - BF
+
+  if (c1 <= 0x7F) // #1 | U+0000   - U+007F, (ASCII)
+    n = 1;
+  else if (0xC2 <= c1 && c1 <= 0xDF &&
+            0x80 <= c2 && c2 <= 0xBF)  // #2 | U+0080   - U+07FF
+    n = 2;
+  else if (0xE0 == c1 &&
+            0xA0 <= c2 && c2 <= 0xBF &&
+            0x80 <= c3 && c3 <= 0xBF)  // #3 | U+0800   - U+0FFF
+    n = 3;
+  else if (0xE1 <= c1 && c1 <= 0xEC &&
+            0x80 <= c2 && c2 <= 0xBF &&
+            0x80 <= c3 && c3 <= 0xBF)  // #4 | U+1000   - U+CFFF
+    n = 3;
+  else if (0xED == c1 &&
+            0x80 <= c2 && c2 <= 0x9F &&
+            0x80 <= c3 && c3 <= 0xBF)  // #5 | U+D000   - U+D7FF
+    n = 3;
+  else if (0xEE <= c1 && c1 <= 0xEF &&
+            0x80 <= c2 && c2 <= 0xBF &&
+            0x80 <= c3 && c3 <= 0xBF)  // #6 | U+E000   - U+FFFF
+    n = 3;
+  else if (0xF0 == c1 &&
+            0x90 <= c2 && c2 <= 0xBF &&
+            0x80 <= c3 && c3 <= 0xBF &&
+            0x80 <= c4 && c4 <= 0xBF)  // #7 | U+10000  - U+3FFFF
+    n = 4;
+  else if (0xF1 <= c1 && c1 <= 0xF3 &&
+            0x80 <= c2 && c2 <= 0xBF &&
+            0x80 <= c3 && c3 <= 0xBF &&
+            0x80 <= c4 && c4 <= 0xBF)  // #8 | U+40000  - U+FFFFF
+    n = 4;
+  else if (0xF4 == c1 &&
+            0x80 <= c2 && c2 <= 0xBF &&
+            0x80 <= c3 && c3 <= 0xBF &&
+            0x80 <= c4 && c4 <= 0xBF)  // #7 | U+10000  - U+3FFFF
+    n = 4;
+  else
+    return std::string::npos; // invalid UTF-8 sequence
+
+  return n;
+}
