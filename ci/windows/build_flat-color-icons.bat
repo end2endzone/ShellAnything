b8cea8396dc342ef2e734df33f679e4d5f9ce019
Antoine Beauchamp <end2endzone@gmail.com>
2024-09-01 11:04:45 -0400

    Fixed incorrect build order in build_windows.yml. Fixed typos in build_flat-color-icons.bat. Also modified script to properly generate 16, 32, 48, and 64 pixels resolutions.

--- ci/windows/build_flat-color-icons.bat
+++ ci/windows/build_flat-color-icons.bat
@@ -3,13 +3,13 @@
 :: Assume that ImageMagick already installed by installation script.
 set PATH=%TEMP%\imagemagick;%PATH%
 
-echo Searching for imagemagik executables in PATH...
+echo Searching for imagemagick executables in PATH...
 where magick.exe
 if errorlevel 1 (
-  echo File 'magick.exe' not found. Please install imagemagik on system and add the directory to PATH.
+  echo File 'magick.exe' not found. Please install imagemagick on system and add the directory to PATH.
   exit /B %errorlevel%
 )
-echo Found imagemagik directory.
+echo Found imagemagick directory.
 echo.
 
 set TEMP_DIR=%TEMP%\flat-color-icons
@@ -20,14 +20,15 @@ set IMAGE_OUTPUT_DIR=%~dp0..\..\build\third_parties\flat-color-icons
 mkdir "%IMAGE_OUTPUT_DIR%" 1>NUL 2>NUL
 mkdir "%TEMP_DIR%" 1>NUL 2>NUL
 
-echo Deleting previous resource file...
-del %IMAGE_OUTPUT_DIR%\resource.rc
+echo Deleting previous resource file %IMAGE_OUTPUT_DIR%\resource.rc...
+del %IMAGE_OUTPUT_DIR%\resource.rc 1>NUL 2>NUL
 echo done.
 echo.
 
 :: For each svg icons
 echo Converting each svg file as ico.
-for /f %%f in ('dir /b "%IMAGE_SRC_DIR%"') do (
+echo Searching for svg files in directory "%IMAGE_INPUT_DIR%"...
+for /f %%f in ('dir /b "%IMAGE_INPUT_DIR%"') do (
   REM echo %%f
   call :process_file "%%f"
 )
@@ -46,19 +47,19 @@ goto :eof
 :: echo INPUT_FILE_PATH IS %IMAGE_INPUT_DIR%\%~1
 :: echo OUTPUT_FILE_PATH IS %IMAGE_OUTPUT_DIR%\%~n1-32.png
 
-:: Notes:
-::   density of 32 --> 16x16 png
-::   density of 48 --> 24x24 png
-::   density of 64 --> 32x32 png
-
 echo Processing file: %~1
-magick -density 32  -background none "%IMAGE_INPUT_DIR%\%~1" "%TEMP_DIR%\%~n1-16.png"
-magick -density 64  -background none "%IMAGE_INPUT_DIR%\%~1" "%TEMP_DIR%\%~n1-32.png"
-magick -density 96  -background none "%IMAGE_INPUT_DIR%\%~1" "%TEMP_DIR%\%~n1-48.png"
-magick -density 128 -background none "%IMAGE_INPUT_DIR%\%~1" "%TEMP_DIR%\%~n1-64.png"
-REM magick -density 192 -background none "%IMAGE_INPUT_DIR%\%~1" "%TEMP_DIR%\%~n1-96.png"
-REM magick -density 256 -background none "%IMAGE_INPUT_DIR%\%~1" "%TEMP_DIR%\%~n1-128.png"
-magick "%TEMP_DIR%\%~n1-16.png" "%TEMP_DIR%\%~n1-32.png" "%TEMP_DIR%\%~n1-48.png" "%TEMP_DIR%\%~n1-64.png" "%IMAGE_OUTPUT_DIR%\%~n1.ico"
+magick -size 16x16 -background none "%IMAGE_INPUT_DIR%\%~1" "%TEMP_DIR%\%~n1-16.png"
+magick -size 32x32 -background none "%IMAGE_INPUT_DIR%\%~1" "%TEMP_DIR%\%~n1-32.png"
+magick -size 48x48 -background none "%IMAGE_INPUT_DIR%\%~1" "%TEMP_DIR%\%~n1-48.png"
+magick -size 64x64 -background none "%IMAGE_INPUT_DIR%\%~1" "%TEMP_DIR%\%~n1-64.png"
+
+set png_files=
+set png_files=%png_files% "%TEMP_DIR%\%~n1-16.png"
+set png_files=%png_files% "%TEMP_DIR%\%~n1-32.png"
+set png_files=%png_files% "%TEMP_DIR%\%~n1-48.png"
+set png_files=%png_files% "%TEMP_DIR%\%~n1-64.png"
+
+magick %png_files% "%IMAGE_OUTPUT_DIR%\%~n1.ico"
 
 goto :eof
 
